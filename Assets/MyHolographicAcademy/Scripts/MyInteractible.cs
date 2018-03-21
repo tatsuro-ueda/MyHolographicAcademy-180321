532cbd3eb3547bf9b10752702e01d7ce162887d8
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2018-03-21 20:28:30 +0900

    ＋MyInteractible スクリプトに新しい機能を追加した

--- Assets/MyHolographicAcademy/Scripts/MyInteractible.cs
+++ Assets/MyHolographicAcademy/Scripts/MyInteractible.cs
@@ -7,10 +7,46 @@ namespace EDUCATION.FEELPHYSICS.MY_HOLOGRAPHIC_ACADEMY
     /// </summary>
     public class MyInteractible : MonoBehaviour
     {
-        #region Public Valuables
+        #region Public Valiables
 
-        [Tooltip("DebugLog オブジェクトをドラッグして下さい")]
-        public GameObject MyDebugLog;
+        [Tooltip("ホログラムにインタラクトしたときに鳴らすオーディオクリップ")]
+        public AudioClip TargetFeedbackSound;
+
+        #endregion
+
+        #region Private Valiables
+
+        /// <summary>
+        /// フォーカスしたときにオーディオクリップを再生するためのもの
+        /// </summary>
+        private AudioSource audioSource;
+
+        /// <summary>
+        /// フォーカスする対象の GameObject に当たっているマテリアルの配列
+        /// </summary>
+        private Material[] defaultMaterials;
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        /// <summary>
+        /// もし Collider がなければ追加する
+        /// ＋オーディオクリップを再生する準備をする
+        /// </summary>
+        private void Start()
+        {
+            this.defaultMaterials = GetComponent<Renderer>().materials;
+
+            // もし Collider がなければ追加する
+            Collider collider = GetComponentInChildren<Collider>();
+            if (collider == null)
+            {
+                gameObject.AddComponent<BoxCollider>();
+            }
+
+            this.EnableAudioHapticFeedback();
+        }
 
         #endregion
 
@@ -21,7 +57,15 @@ namespace EDUCATION.FEELPHYSICS.MY_HOLOGRAPHIC_ACADEMY
         /// </summary>
         public void GazeEntered()
         {
-            DebugLog.Instance.Log += gameObject.name + ": GazeEntered\n";
+            for (int i = 0; i < this.defaultMaterials.Length; i++)
+            {
+                this.SetColorWithEmissionGamma(this.defaultMaterials[i], .02f);
+            }
+
+            if (this.audioSource != null && !this.audioSource.isPlaying)
+            {
+                this.audioSource.Play();
+            }
         }
 
         /// <summary>
@@ -29,7 +73,46 @@ namespace EDUCATION.FEELPHYSICS.MY_HOLOGRAPHIC_ACADEMY
         /// </summary>
         public void GazeExited()
         {
-            DebugLog.Instance.Log += gameObject.name + ": GazeExited\n";
+            for (int i = 0; i < this.defaultMaterials.Length; i++)
+            {
+                this.SetColorWithEmissionGamma(this.defaultMaterials[i], 0f);
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// オーディオクリップを再生するための AudioSource を追加する
+        /// </summary>
+        private void EnableAudioHapticFeedback()
+        {
+            if (this.TargetFeedbackSound != null)
+            {
+                this.audioSource = this.GetComponent<AudioSource>();
+                if (this.audioSource == null)
+                {
+                    this.audioSource = this.gameObject.AddComponent<AudioSource>();
+                }
+
+                this.audioSource.clip = this.TargetFeedbackSound;
+                this.audioSource.playOnAwake = false;
+                this.audioSource.spatialBlend = 1;
+                this.audioSource.dopplerLevel = 0;
+            }
+        }
+
+        /// <summary>
+        /// マテリアルを明るくする
+        /// </summary>
+        /// <param name="material">明るくするマテリアル</param>
+        /// <param name="gamma">どれくらい明るくするか</param>
+        private void SetColorWithEmissionGamma(Material material, float gamma)
+        {
+            var baseColor = material.color;
+            Color finalColor = baseColor * Mathf.LinearToGammaSpace(gamma);
+            material.SetColor("_EmissionColor", finalColor);
         }
 
         #endregion
