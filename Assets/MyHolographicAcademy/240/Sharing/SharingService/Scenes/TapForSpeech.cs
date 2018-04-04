4ac97ce48811c8b32bd8e4d699f63a2c07dc925f
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2018-04-04 11:45:28 +0900

    →240 Sharing：MRTK-Examples のスクリプトが必要なため、Sharing フォルダを入れた

---
+++ Assets/MyHolographicAcademy/240/Sharing/SharingService/Scenes/TapForSpeech.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Sharing.Tests;
+
+public class TapForSpeech : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        GameObject.Find("SyncObjectSpawner").GetComponent<SyncObjectSpawner>().SpawnBasicSyncObject();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
