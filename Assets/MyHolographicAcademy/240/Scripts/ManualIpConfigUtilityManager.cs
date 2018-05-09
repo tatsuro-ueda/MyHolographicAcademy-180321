ff61a95c1f2a42b0682e7e889a0dc87233c0089f
Tatsuro Ueda <tatsuro.ueda@feelphysics.education>
2018-05-09 10:43:17 +0900

    サーバIP手動設定パネルを、サーバに接続したら非表示にし、サーバから切断されたら表示するようにした

---
+++ Assets/MyHolographicAcademy/240/Scripts/ManualIpConfigUtilityManager.cs
@@ -0,0 +1,43 @@
+﻿using HoloToolkit.Sharing;
+using UnityEngine;
+
+/// <summary>
+/// サーバIP手動設定パネルを、サーバに接続したら非表示にし、サーバから切断されたら表示する
+/// </summary>
+public class ManualIpConfigUtilityManager : MonoBehaviour
+{
+    /// <summary>
+    /// サーバIP手動設定パネルのオブジェクト
+    /// 表示・非表示を切り替えるためにUnityのフィールドから設定する
+    /// </summary>
+    public GameObject ManualIpConfigUtility;
+
+    /// <summary>
+    /// サーバに接続したときと、サーバから切断されたときのイベントを準備する
+    /// </summary>
+    private void Start()
+    {
+        SharingStage.Instance.SharingManagerConnected += this.Connected;
+        SharingStage.Instance.SharingManagerDisconnected += this.DisConnected;
+    }
+
+    /// <summary>
+    /// サーバに接続したら、サーバIP手動設定パネルを消す
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Connected(object sender, System.EventArgs e)
+    {
+        this.ManualIpConfigUtility.SetActive(false);
+    }
+
+    /// <summary>
+    /// サーバから切断されたら、サーバIP手動設定パネルを表示する
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void DisConnected(object sender, System.EventArgs e)
+    {
+        this.ManualIpConfigUtility.SetActive(true);
+    }
+}
